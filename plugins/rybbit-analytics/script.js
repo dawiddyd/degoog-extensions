7c39afa9a70b96aab0fc67a72709a49f7cb3ae42
Dawid <d.dydlinski@dydlak.cloud>
2026-06-17 21:55:19 +0200

    Mask page titles

--- plugins/rybbit-analytics/script.js
+++ plugins/rybbit-analytics/script.js
@@ -9,9 +9,24 @@ async function _init() {
     return;
   }
 
+  // Any JS reading document.title (including Rybbit) will always see "degoog",
+  // while direct writes still update the <title> element so the browser tab is unaffected.
+  var _titleEl = document.querySelector("title");
+  var _origTextContent = Object.getOwnPropertyDescriptor(Node.prototype, "textContent");
+  Object.defineProperty(document, "title", {
+    get: function () { return "degoog"; },
+    set: function (val) {
+      if (_titleEl && _origTextContent && _origTextContent.set) {
+        _origTextContent.set.call(_titleEl, val);
+      }
+    },
+    configurable: true,
+  });
+
   var script = document.createElement("script");
   script.src = config.rybbitUrl + "/api/script.js";
   script.setAttribute("data-site-id", config.siteId);
+  script.setAttribute("data-mask-patterns", JSON.stringify(["/search"]));
   script.defer = true;
   document.head.appendChild(script);
 }
